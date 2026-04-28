17b6f45435352fa642c7e0ea5b0894e95a59bf01
bcsaldias <bcsaldias@uc.cl>
2026-04-27 22:13:58 -0700

    Recentered canvas

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,8 +1,11 @@
 // Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
+  const CANVAS_SIZE = 800;
+
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
   };
+
   p.draw = function () {
     p.background(220);
     p.fill(100, 150, 240);
@@ -10,5 +13,6 @@ registerSketch('sk2', function (p) {
     p.textAlign(p.CENTER, p.CENTER);
     p.text('HWK #4. A', p.width / 2, p.height / 2);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function () { p.resizeCanvas(CANVAS_SIZE, CANVAS_SIZE); };
 });
--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,8 +1,11 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+  const CANVAS_SIZE = 800;
+
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
   };
+
   p.draw = function () {
     p.background(240, 200, 200);
     p.fill(180, 60, 60);
@@ -10,5 +13,6 @@ registerSketch('sk3', function (p) {
     p.textAlign(p.CENTER, p.CENTER);
     p.text('HWK #4. B', p.width / 2, p.height / 2);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function () { p.resizeCanvas(CANVAS_SIZE, CANVAS_SIZE); };
 });
